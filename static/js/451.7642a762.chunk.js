3fa96fd32b79dd27cee955b845be8ec9157df773
Anton949432 <133233466+Anton949432@users.noreply.github.com>
2023-12-22 07:25:05 +0000

    Deploying to gh-pages from @ Anton949432/goit-react-hw-05-movies@6d97f2860298924c83bf1f6fbbe23e3d17e1d8fb 🚀

---
+++ static/js/451.7642a762.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[451],{451:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"\u0414\u043e\u043c\u0430\u0448\u043d\u044f \u0441\u0442\u043e\u0440\u0456\u043d\u043a\u0430 \u0437\u0456 \u0441\u043f\u0438\u0441\u043a\u043e\u043c \u043f\u043e\u043f\u0443\u043b\u044f\u0440\u043d\u0438\u0445 \u043a\u0456\u043d\u043e\u0444\u0456\u043b\u044c\u043c\u0456\u0432"})}}}]);
+//# sourceMappingURL=451.7642a762.chunk.js.map
